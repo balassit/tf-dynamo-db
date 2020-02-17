59672ab056654921bfbddde0bcfabf0671401f6c
Blake Lassiter <blake.lassiter@capitalone.com>
2020-02-17 15:35:31 -0800

    terraform 0.12

--- main.tf
+++ main.tf
@@ -1,45 +1,38 @@
+provider "aws" {
+  alias = "us-east-1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias = "us-west-2"
+  region = "us-west-2"
+}
+
 locals {
-  attributes = [
+  attributes = flatten([
     {
-      name = "${var.range_key}"
-      type = "${var.range_key_type}"
+      name = var.range_key
+      type = var.range_key_type
     },
     {
-      name = "${var.partition_key}"
-      type = "${var.partition_key_type}"
-    },
-    ["${var.extra_attributes}"],
-  ]
+      name = var.partition_key
+      type = var.partition_key_type
+    }
+  , var.extra_attributes])
 
-  # Use the `slice` pattern (instead of `conditional`) to remove the first map from the list if no `range_key` is provided
-  # Terraform does not support conditionals with `lists` and `maps`: aws_dynamodb_table.default: conditional operator cannot be used with list values
-  from_index = "${length(var.range_key) > 0 ? 0 : 1}"
+  # Use the slice pattern (instead of conditional) to remove the first map from the list if no range_key is provided
+  # Terraform does not support conditionals with lists and maps: aws_dynamodb_table.default: conditional operator cannot be used with list values
+  from_index = var.range_key == "" ? 1 : 0
 
-  attributes_final = "${slice(local.attributes, local.from_index, length(local.attributes))}"
-}
-
-resource "null_resource" "global_secondary_index_names" {
-  count = "${length(var.global_secondary_index_map)}"
-
-  # Convert the multi-item `global_secondary_index_map` into a simple `map` with just one item `name` since `triggers` does not support `lists` in `maps` (which are used in `non_key_attributes`)
-  # https://www.terraform.io/docs/providers/aws/r/dynamodb_table.html#non_key_attributes-1
-  triggers = "${map("name", lookup(var.global_secondary_index_map[count.index], "name"))}"
-}
-
-resource "null_resource" "local_secondary_index_names" {
-  count = "${length(var.local_secondary_index_map)}"
-
-  # Convert the multi-item `local_secondary_index_map` into a simple `map` with just one item `name` since `triggers` does not support `lists` in `maps` (which are used in `non_key_attributes`)`
-  # https://www.terraform.io/docs/providers/aws/r/dynamodb_table.html#non_key_attributes-1
-  triggers = "${map("name", lookup(var.local_secondary_index_map[count.index], "name"))}"
+  attributes_final = slice(local.attributes, local.from_index, length(local.attributes))
 }
 
 resource "aws_dynamodb_table" "us_east_1_table" {
-  provider     = "aws.us-east-1"
-  hash_key     = "${var.partition_key}"
-  range_key    = "${var.range_key}"
-  billing_mode = "${var.billing_mode}"
-  name         = "${var.table_name}"
+  provider     = aws.us-east-1
+  hash_key     = var.partition_key
+  range_key    = var.range_key
+  billing_mode = var.billing_mode
+  name         = var.table_name
   point_in_time_recovery {
     enabled = true
   }
@@ -50,22 +43,50 @@ resource "aws_dynamodb_table" "us_east_1_table" {
     enabled = true
   }
 
-  attribute              = ["${local.attributes_final}"]
-  global_secondary_index = ["${var.global_secondary_index_map}"]
-  local_secondary_index  = ["${var.local_secondary_index_map}"]
+  dynamic "attribute" {
+    for_each = local.attributes_final
+    content {
+      name = attribute.value.name
+      type = attribute.value.type
+    }
+  }
+  dynamic "global_secondary_index" {
+    for_each = var.global_secondary_index_map
+    content {
+      hash_key           = global_secondary_index.value.hash_key
+      name               = global_secondary_index.value.name
+      non_key_attributes = global_secondary_index.value.non_key_attributes
+      projection_type    = global_secondary_index.value.projection_type
+      range_key          = global_secondary_index.value.range_key
+      read_capacity      = global_secondary_index.value.read_capacity
+      write_capacity     = global_secondary_index.value.write_capacity
+    }
+  }
+  dynamic "local_secondary_index" {
+    for_each = var.local_secondary_index_map
+    content {
+      name               = local_secondary_index.value.name
+      non_key_attributes = local_secondary_index.value.non_key_attributes
+      projection_type    = local_secondary_index.value.projection_type
+      range_key          = local_secondary_index.value.range_key
+    }
+  }
 
-  tags = "${merge(
+
+  tags = merge(
     var.tags,
-    map("Name", "${var.table_name}")
-  )}"
+    {
+      "Name" = var.table_name
+    },
+  )
 }
 
 resource "aws_dynamodb_table" "us_west_2_table" {
-  provider     = "aws.us-west-2"
-  hash_key     = "${var.partition_key}"
-  range_key    = "${var.range_key}"
-  billing_mode = "${var.billing_mode}"
-  name         = "${var.table_name}"
+  provider     = aws.us-west-2
+  hash_key     = var.partition_key
+  range_key    = var.range_key
+  billing_mode = var.billing_mode
+  name         = var.table_name
   point_in_time_recovery {
     enabled = true
   }
@@ -75,20 +96,50 @@ resource "aws_dynamodb_table" "us_west_2_table" {
     enabled = true
   }
 
-  attribute              = ["${local.attributes_final}"]
-  global_secondary_index = ["${var.global_secondary_index_map}"]
-  local_secondary_index  = ["${var.local_secondary_index_map}"]
+  dynamic "attribute" {
+    for_each = local.attributes_final
+    content {
+      name = attribute.value.name
+      type = attribute.value.type
+    }
+  }
+  dynamic "global_secondary_index" {
+    for_each = var.global_secondary_index_map
+    content {
+      hash_key           = global_secondary_index.value.hash_key
+      name               = global_secondary_index.value.name
+      non_key_attributes = global_secondary_index.value.non_key_attributes
+      projection_type    = global_secondary_index.value.projection_type
+      range_key          = global_secondary_index.value.range_key
+      read_capacity      = global_secondary_index.value.read_capacity
+      write_capacity     = global_secondary_index.value.write_capacity
+    }
+  }
+  dynamic "local_secondary_index" {
+    for_each = var.local_secondary_index_map
+    content {
+      name               = local_secondary_index.value.name
+      non_key_attributes = local_secondary_index.value.non_key_attributes
+      projection_type    = local_secondary_index.value.projection_type
+      range_key          = local_secondary_index.value.range_key
+    }
+  }
 
-  tags = "${merge(
+  tags = merge(
     var.tags,
-    map("Name", "${var.table_name}")
-  )}"
+    {
+      "Name" = var.table_name
+    },
+  )
 }
 
 resource "aws_dynamodb_global_table" "global_table" {
-  provider   = "aws.us-east-1"
-  name       = "${var.table_name}"
-  depends_on = ["aws_dynamodb_table.us_east_1_table", "aws_dynamodb_table.us_west_2_table"]
+  provider = aws.us-east-1
+  name     = var.table_name
+  depends_on = [
+    aws_dynamodb_table.us_east_1_table,
+    aws_dynamodb_table.us_west_2_table,
+  ]
 
   replica {
     region_name = "us-east-1"
--- variables.tf
+++ variables.tf
@@ -1,40 +1,41 @@
 variable "partition_key" {
   description = "The key with which to partition (hash) the data."
-  type        = "string"
+  type        = string
 }
 
 variable "partition_key_type" {
   description = "The type of the partitionKey. Valid values are 'S', 'N', or 'B' for (S)tring, (N)umber or (B)inary data"
-  type        = "string"
+  type        = string
   default     = "S"
 }
 
 variable "range_key" {
   description = "The key with which to partition (hash) the data."
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "range_key_type" {
   description = "The type of the partitionKey. Valid values are 'S', 'N', or 'B' for (S)tring, (N)umber or (B)inary data"
-  type        = "string"
+  type        = string
   default     = "S"
 }
 
 variable "enable_autoscaling" {
   description = "The arn of the IAM role to use for autoscaling"
-  type        = "string"
+  type        = string
   default     = "false"
 }
 
 variable "extra_attributes" {
   description = "Additional DynamoDB attributes in the form of a list of mapped values"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "global_secondary_index_map" {
   description = "Additional global secondary indexes in the form of a list of mapped values, eg "
+
   # Example:
   #  global_secondary_index_map = [
   #   {
@@ -48,53 +49,55 @@ variable "global_secondary_index_map" {
   #   },
   #   ...
   # ]
-  type    = "list"
+  type    = list(string)
   default = []
 }
 
 variable "local_secondary_index_map" {
   description = "Additional local secondary indexes in the form of a list of mapped values"
+
   # See global_secondary_index_map for a similar example (specific fields will vary)
-  type    = "list"
+  type    = list(string)
   default = []
 }
 
 variable "autoscaling_role_arn" {
   description = "The arn of the IAM role to use for autoscaling"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "billing_mode" {
   description = "The valid values are PROVISIONED and PAY_PER_REQUEST. Defaults to PROVISIONED."
-  type        = "string"
+  type        = string
   default     = "PAY_PER_REQUEST"
 }
 
 variable "table_name" {
   description = "Name of the dynamo table."
-  type        = "string"
+  type        = string
 }
 
 variable "read_capacity" {
   description = "How much read capacity in units to give to the dynamo tables"
-  type        = "string"
+  type        = string
   default     = "10"
 }
 
 variable "write_capacity" {
   description = "How much write capacity in units to give to the dynamo tables"
-  type        = "string"
+  type        = string
   default     = "10"
 }
 
 variable "env" {
   description = "The environment to deploy e.g. dev"
-  type        = "string"
+  type        = string
 }
 
 variable "tags" {
   description = "A mapping of tags to assign to the object"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
+
